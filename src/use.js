5e3e6ec80e3cdd71ec0913f1a57badcd2d29715a
Ayush Chaturvedi <2200520100019@ietlucknow.ac.in>
2024-04-08 16:39:29 +0530

    sd

--- src/use.js
+++ src/use.js
@@ -12,7 +12,7 @@ export function useMovies(query) {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
           if (!res.ok) throw new Error("Something went wrong");
